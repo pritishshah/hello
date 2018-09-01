dc8591ce4f71daf9133df132c7080230c0f8259c
Pritish Shah <pritish.shah@wework.com>
2018-09-01 15:57:29 -0700

    basic implementation

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/pritishshah/hello
+
+require rsc.io/quote v1.5.2
---
+++ hello.go
@@ -0,0 +1,11 @@
+package main // import "github.com/pritishshah/hello"
+
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
+
+func main() {
+	fmt.Println(quote.Hello())
+}
